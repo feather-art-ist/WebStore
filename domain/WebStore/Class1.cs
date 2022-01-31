c9d0a40d3173a21d2774e344fbf946918d36c1bb
ryhor.by <ryhor.by@outlook.com>
2022-01-31 17:02:38 +0200

    Create class "Book"

---
+++ domain/WebStore/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStore;
+public class Book
+{
+    public int Id { get; }
+
+    public string Title { get; }
+
+    public Book(string title)
+    {
+        Title = title;
+    }
+
+}
